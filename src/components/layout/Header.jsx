8b5dd3e114939aa17c158401e58d32e72b6b5a27
Ramgarhia09 <>
2026-01-13 10:13:30 +0530

    header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -5,12 +5,12 @@ const Header = ({ onNotificationClick }) => {
   return (
     <header className="lg:hidden sticky top-0 z-40 bg-white px-6 py-4 flex items-center justify-between border-b border-gray-200">
       <h1 className="text-2xl font-bold italic text-gray-900">Mini App Store</h1>
-      <button
+      {/* <button
         onClick={onNotificationClick}
         className="p-2 hover:bg-gray-100 rounded-full transition-colors cursor-pointer"
       >
         <Bell size={24} className="text-gray-900" />
-      </button>
+      </button> */}
     </header>
   );
 };
